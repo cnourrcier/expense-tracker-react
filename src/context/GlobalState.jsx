0ddd86097d9eb9fe4e96e2d7c660aacdd53f4205
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-05-08 08:12:42 -0500

    Clean up comments

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -11,9 +11,9 @@ export const GlobalContext = createContext(initialState);
 
 // Provider component
 export const GlobalProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState) // ~19:40
+    const [state, dispatch] = useReducer(AppReducer, initialState)
 
-    // Actions 36:10
+    // Actions
     function deleteTransaction(id) {
         dispatch({
             type: 'DELETE_TRANSACTION',
